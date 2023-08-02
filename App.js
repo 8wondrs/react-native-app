95adbb94d7f94d3c5e42225a550248e093df3c93
8wondrs <8wondrs@gmail.com>
2023-08-02 15:00:44 +0300

    HW2

---
+++ App.js
@@ -0,0 +1,21 @@
+import { useFonts } from 'expo-font';
+import RegistrationScreen from './screens/auth/RegistrationScreen';
+import LoginScreen from './screens/auth/LoginScreen';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+    </>
+  );
+}
